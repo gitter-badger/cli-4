53bda90f4bf8f3bdff6a1681418fc37d72799c92
Karl Grasegger <info@koffeingeladen.de>
2016-06-11 09:24:46 +0200

    Added first version of the needs structure.

---
+++ pet/needs.nim
@@ -0,0 +1,35 @@
+import algorithm, sequtils
+import random, random.mersenne, random.urandom
+
+type
+  Catering * = ref object of RootObj
+    healthyFood: float
+    junkFood: float
+    water: float
+    soda: float
+  Hygiene * = ref object of RootObj
+    shower: float
+    toilet: float
+    maintanance: float
+  Health * = ref object of RootObj
+    doctorVisits: float
+    vaccination: float
+  Care * = ref object of RootObj
+    fun: float
+    love: float
+    training: float
+  Needs = ref object of RootObj
+    care: ref Care
+    catering: ref Catering
+    health: ref Health
+    hygiene: ref Hygiene
+  Baseline * = ref object of Needs
+  Status * = ref object of Needs
+    energy: float
+    knowledge: float
+  Influence * = ref object of Needs
+  Evolution * = ref object of Needs
+
+
+proc newBaseLine * (race: string): Baseline =
+  discard # implement Baseline parser
